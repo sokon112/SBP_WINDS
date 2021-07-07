f0988cfb07683a345fd7c96674fe4bee11f7c0f8
Kwangwoo Nam <skarhkddn123@naver.com>
2021-07-07 16:44:25 +0900

    2021.07.07 16:44 kwangwoo

---
+++ src/main/java/com/spring/od/domain/DeptVO.java
@@ -0,0 +1,17 @@
+package com.spring.od.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeptVO {
+	private int deptno;
+	private String deptName;
+}
