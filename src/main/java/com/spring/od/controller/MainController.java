6f8f568f53373b36152a8f413bccb717218c7105
dayoungpark <xeezee38@gmail.com>
2021-07-12 15:48:48 +0900

    2021.07.12 15:47 dayoung

--- src/main/java/com/spring/od/controller/MainController.java
+++ src/main/java/com/spring/od/controller/MainController.java
@@ -1,6 +1,6 @@
 package com.spring.od.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +13,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Controller
 @Log4j2
-@RequestMapping("/main/*")
+@RequestMapping("/od/*")
 public class MainController {
 
 	@Autowired
@@ -22,10 +22,18 @@ public class MainController {
 	@GetMapping("/")
 	public String odMain() {
 		log.info("od 메인 접속....");
+		service.MainSuccessList();
+		service.MainTempList();
+		service.MainWaitList();
 		return "/od/od_home";
 	}
 	
-//	public String searchList(OfficeNoticeVO vo,Criteria cri) {
-//		//service.
-//	}
+	public String searchList(OfficeNoticeVO vo,Criteria cri) {
+		log.info("검색결과 요청....");
+		
+		
+		
+	}
+
+	
 }
