45fec439e6964c2de6e1be0d0788d2c46f49afc6
simth <simth@DESKTOP-S896JHE>
2021-07-19 14:57:03 +0900

    2021.07.19 14:56 TaeHyeon

--- src/main/java/com/spring/od/domain/StorageListVO.java
+++ src/main/java/com/spring/od/domain/StorageListVO.java
@@ -2,6 +2,17 @@ package com.spring.od.domain;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class StorageListVO {
 	private int docNum;
 	private String title;
